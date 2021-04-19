d4b1df7087a0e19bad30727e1693fb899d0a4cbd
Nacho Divasson <nacho@divasson.com>
2021-04-19 19:24:25 +0200

    cuatro pojos

---
+++ src/main/java/nookin/project/pfinal/model/Direccion.java
@@ -0,0 +1,14 @@
+package nookin.project.pfinal.model;
+
+import lombok.*;
+
+
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode
+public class Direccion {
+    private String direccion;
+    private Double latitud;
+    private Double longitud;
+}
