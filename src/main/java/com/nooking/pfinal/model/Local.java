715e38fea304a6aa1d85e74a8a7826b0c8226511
Alvarogg3 <Alvarogg3@users.noreply.github.com>
2021-05-12 14:53:24 +0200

    Last

---
+++ src/main/java/com/nooking/pfinal/model/Local.java
@@ -0,0 +1,26 @@
+package nookin.project.pfinal.model;
+
+import java.io.File;
+import java.util.List;
+
+import lombok.*;
+
+
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode
+public class Local {
+    private String nombre;
+    private Long id;
+    private Direccion direccion;
+    private String paginaWeb;
+    private String telefono;
+    private List<Valoracion> valoraciones;
+    private Integer aforo;
+    private List<File> imagenes;
+    private Integer visitas;
+    private Integer sem;
+    private List<Categoria> categorias;
+    
+}
---
+++ src/main/java/com/nooking/pfinal/model/Valoracion.java
@@ -0,0 +1,13 @@
+package nookin.project.pfinal.model;
+
+import lombok.*;
+
+
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode
+public class Valoracion {
+    private Integer puntuacion;
+    private String textoValoracion;
+}
